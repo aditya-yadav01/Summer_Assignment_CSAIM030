c8471a4791f1cd756f2c4abd4646e0cc91bc8772
Aditya Yadav <adityayadav@Adityas-MacBook-Air.local>
2026-06-24 10:43:02 +0530

    Day 16

---
+++ Day 16/q63.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, target;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter array elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter target sum: ");
+    scanf("%d", &target);
+
+    int found = 0;
+
+    for(i = 0; i < n; i++) {
+        for(j = i + 1; j < n; j++) {
+            if(arr[i] + arr[j] == target) {
+                printf("Pair found: %d + %d = %d\n",
+                       arr[i], arr[j], target);
+                found = 1;
+            }
+        }
+    }
+
+    if(!found) {
+        printf("No pair found.\n");
+    }
+
+    return 0;
+}
