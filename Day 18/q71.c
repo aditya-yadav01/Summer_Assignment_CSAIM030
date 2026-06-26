6e84ca9c4d83f76902b054c7c52704d9881eac44
Aditya Yadav <adityayadav@Adityas-MacBook-Air.local>
2026-06-26 11:49:33 +0530

    Day 18

---
+++ Day 18/q71.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, key;
+    
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements in sorted order:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the element to search: ");
+    scanf("%d", &key);
+
+    int low = 0, high = n - 1, mid;
+    int found = 0;
+
+    while(low <= high) {
+        mid = (low + high) / 2;
+
+        if(arr[mid] == key) {
+            found = 1;
+            break;
+        }
+        else if(arr[mid] < key) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+
+    if(found)
+        printf("Element found at index %d", mid);
+    else
+        printf("Element not found");
+
+    return 0;
+}
